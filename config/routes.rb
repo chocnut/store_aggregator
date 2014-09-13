7e5001b840b2dce372cfac082838f479ffc10ce9
Peter Indiola <pindiola@gmail.com>
2014-09-13 23:38:41 +0800

    Home Page update

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  root to: 'static_pages#home'
+  root to: 'home_page#index'
 end
--- spec/integration/view_homepage_spec.rb
+++ spec/integration/view_homepage_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 feature 'View the homepage' do
   scenario 'check about the application' do
     visit root_path
-    expect(page).to have_css 'title', text: 'Store Aggregator'
-    expact(page).to have_css '[data-role="description"]'
+    expect(page).to have_title 'Store Aggregator'
+    expect(page).to have_css '[data-role="description"]'
   end
 end

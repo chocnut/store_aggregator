0113915af87782196a5771f68cd8ac6a3f25ae47
Peter Indiola <pindiola@gmail.com>
2014-09-15 05:58:32 +0800

    implemented initial table layout

--- spec/integration/search_store_spec.rb
+++ spec/integration/search_store_spec.rb
@@ -3,8 +3,9 @@ require 'spec_helper'
 feature 'Search store for a term' do
   scenario 'searching by an item name' do
     visit root_path
-    fill_in 'Search', with: 'iPhone5s'
+    fill_in 'store_search', with: 'iPhone5s'
     click_button 'Search'
-    expect(page.all('li.item').count).to be > 1
+    expect(page).to have_css '.table'
+    expect(page.all('.table tr').count).to be > 1
   end
 end
